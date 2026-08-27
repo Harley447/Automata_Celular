79795f64b1a16cae399c029ae5cd4845c0d15c39
Giojan10 <gdcv1006@gmail.com>
2026-08-27 15:57:34 -0500

    Correción de algunas cosas pequeñas y pruebas de cambios secuenciales en la matriz

--- main.cpp
+++ main.cpp
@@ -1,126 +1,193 @@
 #include <iostream>
 #include <cstdlib>
 #include <cstring>
-class Creature {
+#include <random>
 
-  	public:
+class Cell {
+  public:
   	bool alive;
-    int age;
-    Creature(){
-    	alive=true;
-	}
-	
+    virtual ~Cell() {}
 
-    int die() {
+    virtual bool step() {
       return 0;
-    }
-    void move();
-    int getName();
-
+    };
+ 
+    virtual int getName() {
+      return 0;
+    };
 };
 // Clase de presas
-class Prey: public Creature {
+class Prey: public Cell {
   public:
-	const int umbral_breeding=3;
-  	const int max_age=30;
-  	const int max_hijos=4;
+    const int umbralBreeding=3;
+  	const int maxAge=30;
+  	const int maxChildren=4;
+    int age;
+
   	Prey(){ //Necesita la ubicacion en la grilla
-  		age=0;
+  		age = 0;
+      alive = true;
 	  }
-  	void aumentar_Edad(){
+
+  	bool increaseAge(){
   		age++;
-  		if(age>max_age){
-  			die();
-		  }
-	}
-    void reproduction() { //Se reproduce aleatoriamente si el residuo es 0
-      int probabilidad=rand() % 5;
-      if(probabilidad%2==0){
-      	//Requiere conocer todas las 8 celdas contiguas para decidir donde generar en nuevo Prey
-      	// int? free_cell= xyzw
-      	//Prey child(free_cell)
-	  }
+
+      if (age >= maxAge) {
+        return 0;
+      } else {
+        return 1;
+      }
     }
+
     bool canBreed(){
-    	return age >= umbral_breeding;
-	}
-	void move(){
-		aumentar_Edad();
-	}
-	int getName(){
-		return 1;
-	}
+    	return age >= umbralBreeding;
+	  }
+	
+    bool step(){
+		  return increaseAge();
+	  }
+
+	  int getName(){
+		  return 1;
+	  }
 };
 
 // Clases de los depredadores
-class Predator: public Creature {
+class Predator: public Cell {
   public:
-  	const int umbral_breeding=10;
-  	const int max_age=80;
-  	const int max_hijos=2;
-    int energy=4;
-      	
-	Predator(){ //Necesita la ubicacion en la grilla
-  		age=0;
+  	const int umbralBreeding = 10;
+  	const int maxAge = 80;
+  	const int maxChildren = 2;
+    const int startEnergy = 4;
+    int age;
+    int energy;
+
+    Predator(){ //Necesita la ubicacion en la grilla
+  		age = 0;
+      alive = true;
+      energy = startEnergy;
 	  }
-  	void aumentar_Edad(){
+
+  	bool increaseAge() {
   		age++;
-  		if(age>max_age){
-  			die();
-		  }
-	}
-	void aumentar_Hambre(){
-  		energy--;
-  		if(energy<=0){
-  			die();
-		  }
-	}
-    void reproduction() { //Se reproduce aleatoriamente si el residuo es 0
-      int probabilidad=rand() % 8;
-      if(probabilidad%3==0){
-      	//Requiere conocer todas las 8 celdas contiguas para decidir donde generar en nuevo Prey
-      	// int? free_cell= xyzw
-      	//Predator child(free_cell)
-	  }
+  		if(age>maxAge) {
+  			return 0;
+		  } else {
+        return 1;
+      }
     }
+
+    bool increaseHunger() {
+      energy--;
+      if(energy<=0) {
+        return 0;
+      } else {
+        return 1;
+      }
+    }
+
     bool canBreed(){
-    	return age >= umbral_breeding;
-	}
-	void move(){
-		aumentar_Edad();
-	}
-	int getName(){
-		return 2;
-	}
-  };
-class Cell {
-  public:
-    int state;
-    //vector<Creature> animales; 
-    // Hace falta un vector para guardar a creature
-    Creature creature;
-    Cell(int size_grilla){
-    	for(int i=0;i<size_grilla;i++)
-    	
-    	animales.push_back(Creature()); 
-	}
+    	return age >= umbralBreeding;
+	  }
+
+	  bool step(){
+		  return increaseAge() * increaseHunger();
+	  }
+
+	  int getName(){
+		  return 2;
+	  }
 };
 
+class Grid {
+  private:
+    int M, N;
+    Cell** matrix;
+  public:
+    Grid(int width = 10, int height = 10) {
+      M = width;
+      N = height;
+      matrix = new Cell*[M*N];
 
+      for (int i = 0; i < M*N; i++) {
+        switch (rand()%3) {
+          case 0:
+            matrix[i] = new Cell();
+            break;
+          case 1:
+            matrix[i] = new Prey();
+            break;
+          default:
+            matrix[i] = new Predator();
+            break;
+        }
+      }
+    }
+
+    void clean() {
+      // Limpiamos la memoria luego de acabar
+      for (int i = 0; i < M*N; i++) {
+        delete matrix[i];
+      }
+      delete[] matrix;
+    }
+
+    void print() {
+      for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+          std::cout << matrix[i*N+j]->getName();
+        }
+        std::cout << std::endl;
+      }
+    }
+
+    // Se hizo secuencial para evitar definir normas extra :c
+    void step() {
+      int** matrixChecked = new Bool*[M*N]
+      for (int i = 0; i < M; i++) {
+        for (int j =0; j < N; j++) {
+          int index = i * N + j;
+          int type = matrix[index]->getName();
+          
+          matrixChecked[index] = true;
+          // Indices calculados para luego revisarlos :D
+          // En orden son Arriba, Abajo, Izquierda, Derecha
+          int neighbours[4] = {(M*N + (i-1) * N) % (M*N) + j - 1,
+            ((i+1) * N) % M*N + j - 1,
+            i * N + (N + j - 1)%N,
+            i * N + (j + 1) % N};
+          // Muerte por hambre o vejez
+          if (matrix[index]->step() and type != 0) {
+            delete matrix[index];
+            matrix[index] = new Cell();
+          }
+          switch (type) {
+            // Logica de presa
+            case 1:
+              // Revisar si se puede mover
+              int libres = 0;
+              for (int neighbour: neighbours) {
+                if (matrix[neighbour]->getName() == 0) {
+                  libres++;
+                }
+              }
+
+              break;
+            // Logica de cazador
+            case 2:
+              break;
+            // Celda vacia
+            default:
+              break;
+          }
+        }
+      }
+    }
+};
 
 int main() {
-  int M = 10;
-  int N = 10;
-
-  Cell* grid = new Cell[M*N](M*N);
-
-  for (int i = 0; i < M; i++) {
-    for (int j = 0; j < N; j++) {
-      int pos = i * N + j;
-      std::cout << grid[pos].state;
-    }
-    std::cout << std::endl;
-  }
-	free(grid);
+  Grid grid = Grid();
+  grid.print();
+  grid.clean();
   return 0;
 }
